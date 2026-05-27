797a4c22b1e307979cac023f3efd775e729a599e
bobbyu94-netizen <bobbyu94@gmail.com>
2026-05-27 19:00:30 -0400

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -30,10 +30,53 @@ currentPlan.forEach(d => {
     }
   }
 
-  function getNextThursday(){
+  const offset = dayOrder.indexOf(d.day)
+  const eventDate = new Date(startDate)
+  eventDate.setDate(startDate.getDate() + offset)
+
+  eventDate.setHours(plannerSettings.calendarHour || 18,0,0)
+
+  const endDate = new Date(eventDate)
+  endDate.setTime(
+    eventDate.getTime() +
+    ((plannerSettings.calendarDurationHours || 1) * 60 * 60 * 1000)
+  )
+
+  const start = formatICSDate(eventDate)
+  const end = formatICSDate(endDate)
+
+  events += `
+BEGIN:VEVENT
+SUMMARY:${title}
+DTSTART:${start}
+DTEND:${end}
+END:VEVENT
+`
+})
+
+const calendar = `BEGIN:VCALENDAR
+VERSION:2.0
+${events}
+END:VCALENDAR`
+
+const blob = new Blob([calendar], { type: "text/calendar" })
+const url = URL.createObjectURL(blob)
+
+const a = document.createElement("a")
+a.href = url
+a.download = "meal-plan.ics"
+a.click()
+
+hideCalendarPanel();
+
+URL.revokeObjectURL(url)
+
+}
+
+function getNextThursday(){
 
 const today = new Date()
-const day = today.getDay() // 0 = Sunday
+const day = today.getDay()
 
 const THURSDAY = 4
 
@@ -41,8 +84,15 @@ let diff = THURSDAY - day
 
 if(diff <= 0){
   diff += 7
-}                  
+}
 
-  function formatICSDate(date){
+const nextThursday = new Date(today)
+nextThursday.setDate(today.getDate() + diff)
+nextThursday.setHours(0,0,0,0)
+
+return nextThursday
+}
+
+function formatICSDate(date){
 return date.toISOString().replace(/[-:]/g,"").split(".")[0] + "Z"
 }
