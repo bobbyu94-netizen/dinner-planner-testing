47cfd895d00d106d9ccb12421d4f26d2ec4746c7
bobbyu94-netizen <bobbyu94@gmail.com>
2026-05-27 18:50:29 -0400

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,1 +1,31 @@
+function exportToCalendar(){
 
+let events = ""
+
+const startInput = document.getElementById("calendarStartDate").value
+
+if(!startInput){
+  alert("Please choose the Thursday start date first.")
+  return
+}
+
+const startDate = new Date(startInput + "T00:00:00")
+
+const dayOrder = ["Thursday","Friday","Saturday","Sunday","Monday","Tuesday","Wednesday"]
+
+currentPlan.forEach(d => {
+
+  let title = ""
+
+  if(d.dateNight){
+    title = "❤️ Date Night"
+  }
+  else if(d.takeout){
+    title = "Takeout: " + d.takeout
+  }
+  else{
+    title = d.meal
+    if(d.side){
+      title += " + " + d.side
+    }
+  }
