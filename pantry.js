24c889dd3ee7080851a91d3703e23ddabd0cb1dd
bobbyu94-netizen <bobbyu94@gmail.com>
2026-05-27 19:33:07 -0400

    Update pantry.js

--- pantry.js
+++ pantry.js
@@ -1,1 +1,12 @@
+function usePantryItemsForMeal(day){
 
+  const d = currentPlan.find(x => x.day === day);
+
+  if(!d || d.takeout || d.dateNight || !d.items) return;
+
+  d.items.forEach(item => {
+    pantry = pantry.filter(p => p.toLowerCase() !== item.toLowerCase());
+  });
+
+  localStorage.setItem("pantry", JSON.stringify(pantry));
+}
